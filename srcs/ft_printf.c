40ad83b3156952d8b968856ff0773a7cbb32f299
Martin Schneider <mschneid@z4r2p6.le-101.fr>
2018-02-15 16:08:18 +0100

    RGTDGFGDF

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mschneid <mschneid@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/04 13:26:42 by mschneid     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 16:04:32 by mschneid    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/15 16:08:10 by mschneid    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -111,7 +111,6 @@ int				ft_printf(const char *format, ...)
 	output.output = ft_strnew(0);
 	printf("\n- Initial = |%s|\n", nav);
 	va_start(ap, format);
-	printf("%s", nav);
 	while (*nav)
 	{
 		if (*nav == '%')
