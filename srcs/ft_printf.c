4220dcb08c24d050539d691732b4a3e68e08b891
Martin Schneider <mschneid@z4r2p6.le-101.fr>
2018-02-15 14:58:21 +0100

    hehy

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mschneid <mschneid@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/04 13:26:42 by mschneid     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 14:53:39 by mschneid    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/15 14:58:09 by mschneid    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -106,6 +106,7 @@ int				ft_printf(const char *format, ...)
 	t_output		output;
 	int				stop;
 
+	printf("|%s|\n", nav);
 	stop = 0;
 	output.size = 0;
 	output.output = ft_strnew(0);
@@ -127,6 +128,5 @@ int				ft_printf(const char *format, ...)
 	if (stop)
 		return (-1);
 	write(1, output.output, output.size);
-	printf("\n|||||||%d||||||||\n", output.size);
 	return (output.size);
 }
