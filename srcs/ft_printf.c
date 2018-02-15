2c98b41c769d63673ab0814404e64726343174de
Martin Schneider <mschneid@z4r2p6.le-101.fr>
2018-02-15 16:00:32 +0100

    hgfh

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mschneid <mschneid@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/04 13:26:42 by mschneid     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 15:49:01 by mschneid    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/15 16:00:11 by mschneid    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -109,6 +109,7 @@ int				ft_printf(const char *format, ...)
 	stop = 0;
 	output.size = 0;
 	output.output = ft_strnew(0);
+	printf("\n- Initial = |%s|\n", nav);
 	va_start(ap, format);
 	printf("%s", nav);
 	while (*nav)
