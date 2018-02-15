bc2b3aa49c78e428ccc864b8a29b1032b3bddf1a
Martin Schneider <mschneid@z4r2p6.le-101.fr>
2018-02-15 16:13:50 +0100

    satdgf

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mschneid <mschneid@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/04 13:26:42 by mschneid     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 16:08:10 by mschneid    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/15 16:13:42 by mschneid    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -127,7 +127,7 @@ int				ft_printf(const char *format, ...)
 	if (stop)
 		output.size = -1;
 	else
-		write(1, output.output, output.size - 1);
+		write(1, output.output, output.size);
 	free(output.output);
 	return (output.size);
 }
